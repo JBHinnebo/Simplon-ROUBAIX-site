f365f8022a16f3759cdff62096f6dfd9304c59cb
JBH <jean-baptiste@debian-j.baptiste>
2016-10-10 16:56:24 +0200

    création du carroussel

---
+++ js/apps.js
@@ -0,0 +1,14 @@
+//Nous voulons un slide de deux images.
+
+//give the active image, the class "slide_active":
+function disparission (){
+  .play(animation);
+}
+
+//change images with the time that pass
+  //create a timer
+  //create an animation
+    //opacity change
+
+
+//créer un sliders
